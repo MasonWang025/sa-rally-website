cacd07e143f295de700d7d7bcdf6d071fd6954bf
MasonWang025 <mason.wang0025@gmail.com>
2021-04-28 10:49:01 -0700

    color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,31 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        togahacks: {
-          primary: {
-            main: "#C92128",
-            light: "#e8383f",
-            dark: "#a80c12",
+        coc: {
+          light: {
+            light: "##fcf5ff",
+            primary: "#F9EDFF",
+            dark: "##e9d5f2",
           },
-          secondary: {
-            main: "#0b122b",
-            bright: "#101a40",
-            gray: "#141c3b",
-            grayer: "#1a213b",
-            dark: "#050d2b",
+          purple: {
+            light: "#b691d9",
+            primary: "#A880CE",
+            dark: "##8d66b3",
+          },
+          green: {
+            light: "#8cd4a7",
+            primary: "#7BC697",
+            dark: "##63ab7e",
+          },
+          blue: {
+            light: "#8bcfe0",
+            primary: "#74C0D3",
+            dark: "##61aec2",
+          },
+          dark: {
+            light: "##0c3057",
+            primary: "#05203D",
+            dark: "##02101f",
           },
         },
       },
