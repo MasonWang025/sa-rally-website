1e38fec0057502270ef8952e68648bd260096560
MasonWang025 <mason.wang0025@gmail.com>
2021-04-28 11:01:31 -0700

    favicon setup

---
+++ components/utility/Page.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Head from "next/head";
+import Footer from "../global/Footer";
+
+import { GA_TRACKING_ID } from "./gtag";
+
+export default function Page({ fullTitle, title, desc, children }: PageProps) {
+  let pageTitle = fullTitle
+    ? fullTitle
+    : title + " | Stand with Survivors";
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={desc} />
+        <meta property="og:type" content="website" />
+        <meta name="og:title" property="og:title" content={pageTitle} />
+        <meta name="og:description" property="og:description" content={desc} />
+        <meta property="og:site_name" content={pageTitle} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={pageTitle} />
+        <meta name="twitter:description" content={desc} />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#A880CE"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#A880CE" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
+        {/* <meta property="og:image" content="" />
+        <meta name="twitter:image" content="" /> */}
+        {/* google analytics */}
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
+      </Head>
+      {children}
+      <Footer />
+    </>
+  );
+}
+
+type PageProps = {
+  fullTitle?: string;
+  title?: string;
+  desc?: string;
+  children?: React.ReactNode;
+};
