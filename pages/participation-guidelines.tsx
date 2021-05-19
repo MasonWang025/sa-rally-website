688622a91f6626a96cee3b0378eb9c899a525532
MasonWang025 <mason.wang0025@gmail.com>
2021-05-18 21:22:10 -0700

    redirects

---
+++ pages/participation-guidelines.tsx
@@ -0,0 +1,5 @@
+import redirect from "nextjs-redirect";
+
+export default redirect(
+  "https://docs.google.com/document/d/1T0s9gCY1wqxUcAY8uChlqKweOiXeAR1gPZmArsikSdY/edit?usp=sharing"
+);
---
+++ pages/rsvp.tsx
@@ -0,0 +1,5 @@
+import redirect from "nextjs-redirect";
+
+export default redirect(
+  "https://docs.google.com/forms/d/e/1FAIpQLSdeHyeaSQKSOInJlBZ53-HV-eFL2Gf4obr_bVA0C_tWj5zAhw/viewform?usp=sf_link"
+);
