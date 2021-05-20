5fffa86adf3a2c81b21e9ddf25425fe9a30711cb
MasonWang025 <mason.wang0025@gmail.com>
2021-05-20 16:22:08 -0700

    other two speakers

--- components/speakers/SpeakerList.tsx
+++ components/speakers/SpeakerList.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import speakers from "../../data/speakers";
 import Speaker from "./Speaker";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function SpeakerList() {
   return (
@@ -21,6 +23,32 @@ export default function SpeakerList() {
               />
             );
           })}
+          <li className="sm:py-8 sm:text-center">
+            <div className="text-2xl sm:text-3xl font-medium space-y-1">
+              <h3 className="text-4xl text-coc-dark-primary font-bold mb-2">
+                Audrie Pott Foundation <br />
+              </h3>
+              <h3 className="pt-1 text-3xl font-normal text-gray-400 hover:text-gray-500">
+                <a href="https://audriepottfoundation.com/" target="_blank">
+                  <span className="underline break-all">audriepottfoundation.com</span>
+                  <FontAwesomeIcon
+                    icon={faExternalLinkAlt}
+                    className="ml-2 h-6 w-6 inline"
+                  />
+                </a>
+              </h3>
+            </div>
+          </li>
+          <li className="sm:py-8 sm:text-center">
+            <div className="text-2xl sm:text-3xl font-medium space-y-1">
+              <h3 className="text-4xl text-coc-dark-primary font-bold mb-2">
+                Greg Louie <br />
+              </h3>
+              <h3 className="pt-1 text-3xl font-normal text-gray-400 hover:text-gray-500">
+                Saratoga High School Principal
+              </h3>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
